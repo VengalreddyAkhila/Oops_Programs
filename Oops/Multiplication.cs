a939aee19c365704f13a56d0a3a2cfebe9c91743
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-30 21:32:56 +0530

    Added : Calculator

---
+++ Oops/Multiplication.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oops
+{
+    abstract class Calculator
+    {
+        public abstract void Mul(int X, int Y);
+    }
+    class Multiplication : Calculator
+    {
+        public override void Mul(int X, int Y)
+        {
+            int result = X * Y;
+            Console.WriteLine($"Multiplication of {X} * {Y} = {result}");
+        }
+
+    }
+    class Division : Multiplication
+    {
+        public void Div(int X, int Y)
+        {
+            int result = X / Y;
+            Console.WriteLine($"Division of {X} / {Y} = {result}");
+        }
+
+    }
+}
---
+++ Oops/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Oops
+{
+    
+        class Program
+        {
+            public static void Main(String[] args)
+            {
+                Division S = new Division ();
+
+                S.Mul (2 , 5);
+                S.Div(10, 5);
+            }
+        }
+    
+}
+      
