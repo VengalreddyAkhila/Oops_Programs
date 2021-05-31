5ea4574292117ffa8841d3ca775eecb27de64886
VengalreddyAkhila <akhila.it1801@gmail.com>
2021-05-31 21:27:31 +0530

    Added:Example

---
+++ InterfaceExample/Example.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExample
+{
+    interface A
+    {
+        void Hello();
+    }
+    interface B
+    {
+        void Hello();
+    }
+    class HI : A, B
+    {
+        void A.Hello()
+        {
+            Console.WriteLine("Hello to all");
+        }
+        void B.Hello()
+        {
+            Console.WriteLine("Hello to all");
+        }
+    }
+   
+}
+   
---
+++ InterfaceExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InterfaceExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            A Obj1 = new HI();
+            Obj1.Hello();
+            B Obj2 = new HI();
+            Obj2.Hello();
+        }
+    }
+}
